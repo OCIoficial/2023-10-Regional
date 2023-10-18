09add61e7fca428ed0f432b531d9c715c870d530
Nico Lehmann <nico.lehmannm@gmail.com>
2023-10-18 11:21:26 -0400

    [Poleras] Sacar solucion en rust + arreglar solucion en cpp

--- poleras/solutions/correct/nlehmann.cpp
+++ poleras/solutions/correct/nlehmann.cpp
@@ -2,28 +2,21 @@
 #include <cstdio>
 #include <vector>
 
-bool sol(int k, std::vector<int> &a, const std::vector<int> &b) {
+bool sol(int k, std::vector<int> &a, std::vector<int> &b) {
   int n = a.size();
   int j = n - 1;
   for (int i = n - 1; i >= 0; --i) {
-    assert(j - i <= k);
-    while (a[i] < b[i]) {
-      assert(j >= i);
-      if (i == j) {
-        return false;
-      }
-
-      if (a[j] > b[j]) {
-        int s = std::min(b[i] - a[i], a[j] - b[j]);
-        a[i] += s;
+    while (j >= i && b[i] > 0) {
+      if (j - i <= k && a[j] > 0) {
+        int s = std::min(b[i], a[j]);
+        b[i] -= s;
         a[j] -= s;
       } else {
         --j;
       }
     }
-    if (j - i >= k) {
-      --j;
-    }
+    if (b[i] > 0)
+      return false;
   }
   return true;
 }

6ca6751be1a135b459999e08dc5d0d2e98ca5c7f
jpaulsen <juanpablo.paulsen@gmail.com>
2023-10-19 08:38:08 -0700

    Update Jpaulsen.java

--- paltas/solutions/correct/Jpaulsen.java
+++ paltas/solutions/correct/Jpaulsen.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-class JPaulsen {
+public class JPaulsen {
     static int[] count;
     static ArrayList<Pair> good = new ArrayList<>();
     static ArrayList<Pair> bad = new ArrayList<>();

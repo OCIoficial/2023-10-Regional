c9b32847d7854c49dca32b6ed2fa3dff928d78d5
jpaulsen <juanpablo.paulsen@gmail.com>
2023-10-19 08:39:18 -0700

    Update Jpaulsen.java

--- piratas/solutions/correct/Jpaulsen.java
+++ piratas/solutions/correct/Jpaulsen.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class Jpaulsen {
+class piratas {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
